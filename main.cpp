53d19613dc91428aa5e731122d76b5307adfca45
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2017-11-24 23:03:07 +0100

    new version

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
+#include <cstdlib>
+#include <string>
 #include "MNISTread.h"
-#include "knn_functions.h"
+#include "KNNfunctions.h"
+//wyswietlanie vectorow
+
 
 int main()
 {
-	std::string filename = "mnist/t10k-images-idx3-ubyte";
+	std::string filename_images_10k = "t10k-images-idx3-ubyte.gz";
 	int number_of_images = 10000;
 	int image_size = 28 * 28;
 
-	std::vector<std::vector<double> > vec1;
-	read_Mnist(filename, vec1);
-	std::cout << vec1.size() << std::endl;
-	std::cout << vec1[0].size() << std::endl;
+	std::vector<std::vector<double> > vec_images_10k;
+	read_Mnist(filename_images_10k, vec_images_10k);
+	std::cout << vec_images_10k.size() << std::endl;
+	//std::cout << vec_images_10k[0].size() << std::endl;
 
 
 	
 	//read MNIST label into double vector
-	std::string filename = "mnist/t10k-labels-idx1-ubyte";
-	int number_of_images = 10000;
+	std::string filename_label_10k = "t10k-labels-idx1-ubyte.gz";
+	int number_of_labels = 10000;
 
 
-	std::vector<double> vec(number_of_images);
-	read_Mnist_Label(filename, vec);
-	std::cout << vec.size() << std::endl;
-
+	std::vector<double> vec_label_10k; // (number_of_labels);
+	read_Mnist_Label(filename_label_10k, vec_label_10k);
+	std::cout << vec_label_10k.size() << std::endl;
+	
+	system("pause");
 	return 0;
 }
