7c88a9911ff046dd5d78513992636ccd27c56a02
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2017-11-25 20:34:38 +0100

    working reading for MNIST files

--- MNISTread.cpp
+++ MNISTread.cpp
@@ -16,61 +16,70 @@ int ReverseInt(int i)
 void read_Mnist(std::string filename, std::vector<std::vector<double> > &vec)
 {
 	std::ifstream file(filename, std::ios::binary);
-		if (file.is_open())	
+	if (file.is_open())
+	{
+		int magic_number = 0;
+		int number_of_images = 0;
+		int n_rows = 0;
+		int n_cols = 0;
+
+		file.read((char*)&magic_number, sizeof(magic_number));
+		magic_number = ReverseInt(magic_number);
+		file.read((char*)&number_of_images, sizeof(number_of_images));
+		number_of_images = ReverseInt(number_of_images);
+		file.read((char*)&n_rows, sizeof(n_rows));
+		n_rows = ReverseInt(n_rows);
+		file.read((char*)&n_cols, sizeof(n_cols));
+		n_cols = ReverseInt(n_cols);
+
+		for (int i = 0; i < number_of_images; ++i)
 		{
-				int magic_number = 0;
-				int number_of_images = 0;	
-				int n_rows = 0;
-				int n_cols = 0;
-		
-				file.read((char*)&magic_number, sizeof(magic_number));
-				magic_number = ReverseInt(magic_number);
-				file.read((char*)&number_of_images, sizeof(number_of_images));
-				number_of_images = ReverseInt(number_of_images);
-				file.read((char*)&n_rows, sizeof(n_rows));
-				n_rows = ReverseInt(n_rows);
-				file.read((char*)&n_cols, sizeof(n_cols));
-				n_cols = ReverseInt(n_cols);
-			
-				for (int i = 0; i < number_of_images; ++i)	
+			std::vector<double> tp;
+			for (int r = 0; r < n_rows; ++r)
+			{
+				for (int c = 0; c < n_cols; ++c)
 				{
-					std::vector<double> tp;
-					for (int r = 0; r < n_rows; ++r)	
-					{
-						for (int c = 0; c < n_cols; ++c)
-						{
-							unsigned char temp = 0;
-							file.read((char*)&temp, sizeof(temp));
-							tp.push_back((double)temp);	
-						}	
-					}
-					vec.push_back(tp);			
-				}		
+					unsigned char temp = 0;
+					file.read((char*)&temp, sizeof(temp));
+					tp.push_back((double)temp);
+				}
+			}
+			vec.push_back(tp);
 		}
+	}
+	else
+		std::cout << "unable to open file" << std::endl;
 }
 
 
 void read_Mnist_Label(std::string filename, std::vector<double> &vec)
 {
-	std::ifstream file (filename, std::ios::binary);
+	std::ifstream file (filename);
     if (file.is_open())
     {
         int magic_number = 0;
         int number_of_images = 0;
-        int n_rows = 0;
-        int n_cols = 0;
+
 
         file.read((char*) &magic_number, sizeof(magic_number));
         magic_number = ReverseInt(magic_number);
+		if (magic_number != 2049) 
+			std::cout<<"Invalid MNIST image file!"<<std::endl;
         file.read((char*) &number_of_images,sizeof(number_of_images));
         number_of_images = ReverseInt(number_of_images);
 
         for(int i = 0; i < number_of_images; ++i)
 
         {
+			double tp = 0;
             unsigned char temp = 0;
             file.read((char*) &temp, sizeof(temp));
-            vec[i]= (double)temp;
+            tp = (double)temp;
+			vec.push_back(tp);
         }
+		file.close();
     }
+	else
+		std::cout << "unable to open file" << std::endl;
 }
+
